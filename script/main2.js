21420813672d05fa549bb639f9b6ff7a42da26f8
ccrespoh <carolinach@me.com>
2015-05-12 15:37:15 -0400

    creation

---
+++ script/main2.js
@@ -0,0 +1,32 @@
+/*
+ function toggle(id){
+ */
+/*if (actual != null) document.getElementById(actual).style.display="none";
+ actual=id;*//*
+
+ */
+/*document.getElementById(id).style.display="inline";*//*
+
+ </div>.style="visible";
+ var links=getElementsByClassName("contents").id;
+ for(var i=0;i<link.length;i++){
+ if(links[i].id=="id"){
+ style="visible";
+ }
+ }
+ var vis=document.getElementById(id);
+
+ console.log(vis);
+ }*/
+
+
+'use strict';
+$(document).ready(function () {
+    $(".fermer").click(function () {
+        this.parentNode.style.display = "none";
+    });
+});
+
+function afficher(clique){
+    document.getElementById(clique).style.display='block';
+}
